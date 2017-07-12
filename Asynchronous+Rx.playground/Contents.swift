d23450a40efebe591800a5ffc4454a682ebc19da
takumi314 <isospin_symmetry@me.com>
2017-07-13 08:45:08 +0900

    ready for learning Asynchronous+Rx

---
+++ Asynchronous+Rx.playground/Contents.swift
@@ -0,0 +1,50 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import RxSwift
+import RxCocoa
+import PlaygroundSupport
+
+
+/*
+ 
+ # Intro to Schedulers
+ 
+ - This chapter will cover the beauty behind schedulers, 
+ where you’ll learn why the Rx abstraction is so powerful and 
+ why working with 'asynchronous' programming is far less less 
+ painful than using locks or queues.
+
+
+ */
+
+
+
+struct Session {
+    static func send<T: Request>(_ request: T) -> Observable<T.Response> {
+        let observable = Observable<T.Response>.create { observer in
+            let task = Session.send(request, callbackQueue: .main, handler: { result in
+                switch result {
+                case .success(let value):
+                    observer.on(.next(value))
+                    observer.onCompleted()
+
+                case .failure(let error):
+                    if (error as NSError).code == URLError.cancelled.rawValue {
+                        observer.onCompleted()
+                        break
+                    }
+                    observer.onError(error)
+                }
+            })
+            return Disposables.create(with: {
+                task?.cancel()
+            })
+        }
+        return observable.take(1)
+    }
+
+    static func cancelRequest<T: Request>(with type: T.Type) {
+        Session.cancelRequests(with: type)
+    }
+}
--- Operator.playground/Contents.swift
+++ Operator.playground/Contents.swift
@@ -6,3 +6,46 @@ import RxSwift
 import PlaygroundSupport
 
 
+/*:
+ 
+ # RxSwift's Operators
+
+ */
+
+
+
+
+/*:
+ 
+ ## 1.Filtering Operators
+
+ */
+
+
+
+
+
+/*:
+
+ ## 2.Transforming Operators
+
+ */
+
+
+
+
+
+/*:
+
+ ## 3.Combining Operators
+
+ */
+
+
+/*:
+
+ ## 4.Time Based Operators
+ 
+ */
+
+
